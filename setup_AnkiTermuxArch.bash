932570f5e8b60426c74a507facf5df2810976301
sudomain <>
2020-04-21 16:53:22 -0400

    update files

--- setup_AnkiTermuxArch.bash
+++ setup_AnkiTermuxArch.bash
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-set -Eeuo pipefail
+set -uo pipefail
 
 # Dependencies for TermuxArch
 pkg install bsdtar proot
@@ -16,6 +16,14 @@ startarch cmd 'pacman -Syu'
 startarch cmd 'pacman -S xfce4 tigervnc anki'
 
 # Anki wont run as root so we'll create a new user in the proot
-startarch cmd 'addauser vncuser'
+startarch cmd 'useradd --create-home vncuser'
 
-# 
+# Copy the start and stop scripts. These can be started via the shell or a Termux shortcut
+cp config_files/arch/bin/start-vnc-server ~/arch/home/vncuser/bin/
+cp config_files/arch/bin/stop-vnc-server ~/arch/home/vncuser/bin/
+
+# Copy the Termux shortcut wrapper scripts
+cp config_files/termux-shortcuts/startarch-gui ~/.shortcuts/
+cp config_files/termux-shortcuts/stoparch-gui ~/.shortcuts/
+
+
